76a09e35689d686ca1d1149b770d9b03ccc45d99
Tom Weisansal <trweisa@gmail.com>
2022-02-12 11:53:35 -0500

    Initial commit

---
+++ public/socketStuff.js
@@ -0,0 +1,60 @@
+// THIS IS CLIENT
+
+let socket = io.connect('http://localhost:8080')
+
+function init(){
+    draw();
+    socket.emit('init',{
+        playerName: player.name
+    });
+}
+
+socket.on('initReturn',(data)=>{
+    orbs = data.orbs;
+    setInterval(()=>{
+        socket.emit('tick',{
+            xVector: player.xVector,
+            yVector: player.yVector
+        });
+    },33);
+});
+
+socket.on('tock',(data)=>{
+    players = data.players;
+});
+
+socket.on('orbSwitch',(data)=>{
+    orbs.splice(data.orbIndex,1,data.newOrb);
+});
+
+socket.on('tickTock',(data)=>{
+    player.locX = data.playerX;
+    player.locY = data.playerY;
+});
+
+socket.on('updateLeaderBoard',(data)=>{
+    // console.log(data);
+    document.querySelector('.leader-board').innerHTML = "";
+    data.forEach((curPlayer)=>{
+        document.querySelector('.leader-board').innerHTML += `
+            <li class="leaderboard-player">${curPlayer.name} - ${curPlayer.score}</li>
+        `;
+    });
+});
+
+socket.on('playerDeath',(data)=>{
+    document.querySelector('#game-message').innerHTML = `${data.died.name} absorbed by ${data.killedBy.name}`;
+    $("#game-message").css({
+        "background-color": "#00e6e6",
+        "opacity": 1
+    });
+    $("#game-message").show();
+    $("#game-message").fadeOut(5000);
+
+    if(player.uid == data.died.uid)
+    {
+        player.isAlive = false;
+        $('#spawnModal').modal('show');
+        document.querySelector('.stats-wrapper').innerHTML = `Total Orb count: ${data.died.orbsAbsorbed}`;
+    }
+});
---
+++ sockets/socketMain.js
@@ -0,0 +1,127 @@
+// this is the server
+const io = require('../servers').io;
+const checkForOrbCollisions = require('./checkCollisions').checkForOrbCollisions;
+const checkForPlayerCollisions = require('./checkCollisions').checkForPlayerCollisions;
+
+const Player = require('./classes/Player');
+const PlayerData = require('./classes/PlayerData');
+const PlayerConfig = require('./classes/PlayerConfig');
+const Orb = require('./classes/Orb');
+let orbs = [];
+let players = [];
+let settings = {
+    defaultOrbs: 50,
+    defaultSpeed: 6,
+    defaultSize: 6,
+    defaultZoom: 1.5,
+    worldWidth: 500,
+    worldHeight: 500
+};
+
+initGame();
+
+setInterval(()=>{
+    if(players.length > 0){
+        io.to('game').emit('tock',{
+            players,
+        });
+    }
+},33);
+
+io.sockets.on('connect',(socket)=>{
+    let player = {};
+    socket.on('init',(data)=>{
+        // add the player to the game namespace
+        socket.join('game');
+        let playerConfig = new PlayerConfig(settings);
+        let playerData = new PlayerData(data.playerName,settings);
+        player = new Player(socket.id, playerConfig, playerData);
+
+        // issue a message to THIS client with it's loc 30/sec
+        setInterval(()=>{
+            socket.emit('tickTock',{
+                playerX: player.playerData.locX,
+                playerY: player.playerData.locY,
+            });
+        },33);
+
+        socket.emit('initReturn',{
+            orbs
+        });
+        players.push(playerData);
+    })
+    
+    socket.on('tick',(data)=>{
+        speed = player.playerConfig.speed
+        // update with new direction
+        xV = player.playerConfig.xVector = data.xVector;
+        yV = player.playerConfig.yVector = data.yVector;
+    
+        if((player.playerData.locX < 5 && player.playerData.xVector < 0) || 
+            (player.playerData.locX > settings.worldWidth) && 
+            (xV > 0))
+        {
+            player.playerData.locY -= speed * yV;
+        }
+        else if((player.playerData.locY < 5 && yV > 0) || 
+            (player.playerData.locY > settings.worldHeight) && 
+            (yV < 0))
+        {
+            player.playerData.locX += speed * xV;
+        }
+        else{
+            player.playerData.locX += speed * xV;
+            player.playerData.locY -= speed * yV;
+        }
+
+        let capturedOrb = checkForOrbCollisions(player.playerData,player.playerConfig,orbs,settings);
+        capturedOrb.then((data)=>{
+            const orbData = {
+                orbIndex: data,
+                newOrb: orbs[data]
+            }
+            
+            io.sockets.emit('updateLeaderBoard',getLeaderBoard());
+            io.sockets.emit('orbSwitch',orbData);
+        }).catch(()=>{})
+
+        let playerDeath = checkForPlayerCollisions(player.playerData,player.playerConfig,players,player.socketId)
+        playerDeath.then((data)=>{
+            io.sockets.emit('updateLeaderBoard',getLeaderBoard());
+            io.sockets.emit('playerDeath',data);
+        }).catch(()=>{})
+    });
+
+    socket.on('disconnect',(data)=>{
+        if(player.playerData){
+            players.forEach((currPlayer,i)=>{
+                if(currPlayer.uid == player.playerData.uid){
+                    players.splice(i,1);
+                    io.sockets.emit('updateLeaderBoard',getLeaderBoard());
+                }
+            });
+        }
+    })
+})
+
+function getLeaderBoard(){
+    players.sort((a,b)=>{
+        return b.score - a.score;
+    });
+
+    let leaderBoard = players.map((curPlayer)=>{
+        return{
+            name: curPlayer.name,
+            score: curPlayer.score
+        };
+    })
+    return leaderBoard;
+}
+
+function initGame(){
+    for(let i = 0; i < settings.defaultOrbs; i++){
+        orbs.push(new Orb(settings));
+    }
+}
+
+module.exports = io
